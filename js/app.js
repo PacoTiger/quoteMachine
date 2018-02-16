4759b5dda85690aae7d1d5ccd4268583521da629
Paco Padilla <padilla.fco@gmail.com>
2018-02-16 17:55:52 +0800

    Making Http Requests to Public API
    with JS vainilla

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-app.js
+class Quote {
+	constructor (phrase, author){
+		this.phrase = phrase;
+		this.author = author;
+	}
+}
+
+//Variables
+const URL = "https://talaikis.com/api/quotes/random/";
+let xRequest = new XMLHttpRequest();
+
+//HTTP Request
+xRequest.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		let phrase = JSON.parse(xRequest.responseText).quote;
+		let author = JSON.parse(xRequest.responseText).author;
+		let quote = new Quote(phrase, author);
+		console.log(quote);
+	}
+}
+
+//Trigger
+function makeRequest() {
+	xRequest.open("GET", URL);
+    xRequest.send();
+}
