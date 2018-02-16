7ff2c86229b34afa9efaeac14a494933e74e5a02
Paco Padilla <padilla.fco@gmail.com>
2018-02-17 02:14:34 +0800

    new update.

--- js/app.js
+++ js/app.js
@@ -15,12 +15,30 @@ xRequest.onreadystatechange = function() {
 		let phrase = JSON.parse(xRequest.responseText).quote;
 		let author = JSON.parse(xRequest.responseText).author;
 		let quote = new Quote(phrase, author);
-		console.log(quote);
+		displayQuote(quote);
 	}
 }
 
-//Trigger
+
+function displayQuote(quote) {
+	document.getElementById('phrase').innerHTML = quote.phrase;
+	document.getElementById('author').innerHTML = quote.author;	
+}
+
 function makeRequest() {
 	xRequest.open("GET", URL);
     xRequest.send();
 }
+
+//The recommended way and equivalent to $(document).ready(funtion(){});
+$(function() {
+	//In order download the first quote
+	makeRequest();
+
+	//TRIGGERS
+	//When press phrase or author text.
+	$('.quote').on("click", makeRequest);
+
+	//When press twitter icon.
+	//Code here.
+});
