63bc7734f98f39fcb7c862d57ecc32ba35ec0bf9
Paco Padilla <padilla.fco@gmail.com>
2018-02-17 21:22:20 +0800

    Icon to sharetwitter working
    just need animations

--- js/app.js
+++ js/app.js
@@ -1,44 +1,55 @@
+//The recommended way and equivalent to $(document).ready(funtion(){});
+$(function() {
+//Global variables
+	const URL = "https://talaikis.com/api/quotes/random/";
+	let xRequest = new XMLHttpRequest();
+	var quote;
+
+//Should be modularized it
 class Quote {
 	constructor (phrase, author){
 		this.phrase = phrase;
 		this.author = author;
 	}
-}
 
-//Variables
-const URL = "https://talaikis.com/api/quotes/random/";
-let xRequest = new XMLHttpRequest();
-
-//HTTP Request
-xRequest.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		let phrase = JSON.parse(xRequest.responseText).quote;
-		let author = JSON.parse(xRequest.responseText).author;
-		let quote = new Quote(phrase, author);
-		displayQuote(quote);
+	shareTwitter() {
+		openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + this.phrase + '" ' + this.author));
 	}
 }
 
+//FUNCTIONS
+	function displayQuote(quote) {
+		document.getElementById('phrase').innerHTML = quote.phrase;
+		document.getElementById('author').innerHTML = quote.author;	
+	}
 
-function displayQuote(quote) {
-	document.getElementById('phrase').innerHTML = quote.phrase;
-	document.getElementById('author').innerHTML = quote.author;	
-}
+	function makeRequest() {
+		xRequest.open("GET", URL);
+	    xRequest.send();
+	}
 
-function makeRequest() {
-	xRequest.open("GET", URL);
-    xRequest.send();
-}
+	function openURL(url){
+	  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+	}
 
-//The recommended way and equivalent to $(document).ready(funtion(){});
-$(function() {
-	//In order download the first quote
-	makeRequest();
+	//HTTP Request
+	xRequest.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let phrase = JSON.parse(xRequest.responseText).quote;
+			let author = JSON.parse(xRequest.responseText).author;
+			quote = new Quote(phrase, author);
+			displayQuote(quote);	
+		}
+	}
 
-	//TRIGGERS
+//TRIGGERS
 	//When press phrase or author text.
 	$('.quote').on("click", makeRequest);
 
 	//When press twitter icon.
-	//Code here.
+	$('#twitterIcon').on("click", function() {
+		return quote.shareTwitter();
+	});
+	//In order to download the first quote
+	makeRequest();
 });
